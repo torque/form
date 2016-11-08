218bec45866e5e95be8ab7a7fc44629a9448e0cb
jonathanh137 <jonathanh137@gmail.com>
2016-11-08 12:02:52 -0800

    Update source.php

--- source.php
+++ source.php
@@ -16,7 +16,24 @@
 		Advisor: <input type="text" required name="advisor"><br>
 		Advisor Email: <input type="email" required name="ademail"><br>
 		<h2>Position Data:</h2>
-		TA:<input type="radio" name="atype" value="TA"> RA:<input type="radio" name="atype" value="RA"><br>
+		<script type="text/javascript">
+		  function yesnoCheck() {
+		    if (document.getElementById('RA').checked) {
+			document.getElementById('RAYes').style.display = 'block';
+		    }
+		    else document.getElementById('RAYes').style.display = 'none';
+		  }
+		</script>
+		TA: <input type="radio" onclick="javascript:yesnoCheck();" name="atype" id="TA" value="TA"> RA: <input type="radio" onclick="javascript:yesnoCheck();" name="atype" id="RA" value="RA"><br>
+		<div id="RAYes" style="display:none">
+		  Account:<input type='text' id='RAAcc' name='RAAcc'>
+		  Fund:<input type='text' id='RAFund' name='RAFund'>
+		  Dept:<input type='text' id='RADept' name='RADept'>
+		  Pgm Code:<input type='text' id='RAcode' name='RAcode'>
+		  Activity:<input type='text' id='RAAct' name='RAAct'>
+		  Class:<input type='text' id='RAClass' name='RAClass'>
+		  Project ID:<input type='text' id='RAId' name='RAId'>
+		</div>
 		Academic Year: <input type="text" required name="year"><br>
 		Quarter: <input type="text" required name="quarter"><br>
 		Hiring Dept/Pgm: <input type="text" required name="hdept"><br>

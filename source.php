160a7da92f7c27ca12dc239c834f784cd6e131d5
Marton Demeter <marton1029@gmail.com>
2016-11-18 14:30:16 -0800

    Added minor improvements

--- source.php
+++ source.php
@@ -23,37 +23,37 @@
 
 		<h3>Student Data</h3>
 
-		<div class = "row">
+		<div class = "row" style="text-align:center">
 			<div class ="col-sm-3">
 				<h5>First Name</h5>
-				<input type="text" required name="first_name"><br>
+				<input type="text" required name="first_name" placeholder="First Name"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>Last Name</h5>
-				<input type="text" required name="last_name"><br>
+				<input type="text" required name="last_name" placeholder="Last Name"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>SCU ID</h5>
-				<input type="text" required name="id"><br>
+				<input type="text" required name="id" placeholder="SCU ID"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>Major</h5>
-				<input type="text" required name="major"><br>
+				<input type="text" required name="major" placeholder="Major"><br>
 			</div>
 		</div>
 
-		<div class ="row">
+		<div class ="row" style="text-align:center">
 			<div class ="col-sm-3">
 				<h5>Student Email</h5>
-				<input type="email" required name="email"><br>
+				<input type="email" required name="email" placeholder="Student Email"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>Advisor</h5>
-				<input type="text" required name="advisor"><br>
+				<input type="text" required name="advisor" placeholder="Advisor"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>Advisor Email</h5>
-				<input type="email" required name="ademail"><br>
+				<input type="email" required name="ademail" placeholder="Advisor Email"><br>
 			</div>
 		</div>
 
@@ -96,13 +96,96 @@
                 }
                 document.getElementById('total').value = tot;
             }
+            function addClass() {
+                if(document.getElementById('class_2').style.display == "none") {
+                    if(document.getElementById('cid1').value != '' && document.getElementById('ctitle1').value != '' && document.getElementById('credit1').value != '') {
+                        document.getElementById('class_2').style.display = "block";
+                        document.getElementById('class_2').style.textAlign="center";
+                        document.getElementById('class_2_break').style.display = "block";
+                    }
+                    return;
+                }
+                if(document.getElementById('class_3').style.display == "none") {
+                    if(document.getElementById('cid2').value != '' && document.getElementById('ctitle2').value != '' && document.getElementById('credit2').value != '') {
+                        document.getElementById('class_3').style.display = "block";
+                        document.getElementById('class_3').style.textAlign="center";
+                        document.getElementById('class_3_break').style.display = "block";
+                    }
+                    return;
+                }
+                if(document.getElementById('class_4').style.display == "none") {
+                    if(document.getElementById('cid3').value != '' && document.getElementById('ctitle3').value != '' && document.getElementById('credit3').value != '') {
+                        document.getElementById('class_4').style.display = "block";
+                        document.getElementById('class_4').style.textAlign="center";
+                        document.getElementById('class_4_break').style.display = "block";
+                    }
+                    return;
+                }
+                if(document.getElementById('class_5').style.display == "none") {
+                    if(document.getElementById('cid4').value != '' && document.getElementById('ctitle4').value != '' && document.getElementById('credit4').value != '') {
+                        document.getElementById('class_5').style.display = "block";
+                        document.getElementById('class_5').style.textAlign="center";
+                        document.getElementById('class_5_break').style.display = "block";
+                    }
+                    return;
+                }
+                if(document.getElementById('class_6').style.display == "none") {
+                    if(document.getElementById('cid5').value != '' && document.getElementById('ctitle5').value != '' && document.getElementById('credit5').value != '') {
+                        document.getElementById('class_6').style.display = "block";
+                        document.getElementById('class_6').style.textAlign="center";
+                        document.getElementById('class_6_break').style.display = "block";
+                    }
+                    return;
+                }
+            }function removeClass() {
+                if(document.getElementById('class_6').style.display == "block") {
+                    document.getElementById('class_6').style.display = "none";
+                    document.getElementById('cid6').value='';
+                    document.getElementById('ctitle6').value='';
+                    document.getElementById('credit6').value='';
+                    document.getElementById('class_6_break').style.display = "none";
+                    return;
+                }
+                if(document.getElementById('class_5').style.display == "block") {
+                    document.getElementById('class_5').style.display = "none";
+                    document.getElementById('cid5').value='';
+                    document.getElementById('ctitle5').value='';
+                    document.getElementById('credit5').value='';
+                    document.getElementById('class_5_break').style.display = "none";
+                    return;
+                }
+                if(document.getElementById('class_4').style.display == "block") {
+                    document.getElementById('class_4').style.display = "none";
+                    document.getElementById('cid4').value='';
+                    document.getElementById('ctitle4').value='';
+                    document.getElementById('credit4').value='';
+                    document.getElementById('class_4_break').style.display = "none";
+                    return;
+                }
+                if(document.getElementById('class_3').style.display == "block") {
+                    document.getElementById('class_3').style.display = "none";
+                    document.getElementById('cid3').value='';
+                    document.getElementById('ctitle3').value='';
+                    document.getElementById('credit3').value='';
+                    document.getElementById('class_3_break').style.display = "none";
+                    return;
+                }
+                if(document.getElementById('class_2').style.display == "block") {
+                    document.getElementById('class_2').style.display = "none";
+                    document.getElementById('cid2').value='';
+                    document.getElementById('ctitle2').value="";
+                    document.getElementById('credit2').innerHTML="";
+                    document.getElementById('class_2_break').style.display = "none";
+                    return;
+                }
+            }
 		</script>
 
 		TA <input type="radio" onclick="javascript:yesnoCheck();" name="atype" id="TA" value="TA">
 		RA <input type="radio" onclick="javascript:yesnoCheck();" name="atype" id="RA" value="RA"><br>
         <input type="hidden" name="is_ra" value="false" id="is_ra"/>
         <div id="TAYes" style="display:none">
-            <div class="row">
+            <div class="row" style="text-align:center">
                <div class ="col-sm-3">
                     <select>
                         <option value="Full Time">Full Time</option>
@@ -113,36 +196,36 @@
             </div>
         </div>
 		<div id="RAYes" style="display:none">
-			<div class = "row">
+			<div class = "row" style="text-align:center">
 				<div class ="col-sm-3">
 					<h5>Account</h5>
-					<input type='text' id='RAAcc' name='RAAcc'>
+					<input type='text' id='RAAcc' name='RAAcc' placeholder="Account">
 				</div>
 				<div class ="col-sm-3">
 					<h5>Fund</h5>
-				  	<input type='text' id='RAFund' name='RAFund'>
+				  	<input type='text' id='RAFund' name='RAFund' placeholder="Fund">
 			  	</div>
 			  	<div class ="col-sm-3">
 					<h5>Dept.</h5>
-				  	<input type='text' id='RADept' name='RADept'>
+				  	<input type='text' id='RADept' name='RADept' placeholder="Department">
 			  	</div>
 			  	<div class ="col-sm-3">
 				  	<h5>Prgm. Code</h5>
-				  	<input type='text' id='RAcode' name='RAcode'>
+				  	<input type='text' id='RAcode' name='RAcode' placeholder="Prgm. Code">
 			  	</div>
 		 	</div>
-		 	<div class ="row">
+		 	<div class ="row" style="text-align:center">
 			  	<div class ="col-sm-3">
 				  	<h5>Activity</h5>
-				  	<input type='text' id='RAAct' name='RAAct'>
+				  	<input type='text' id='RAAct' name='RAAct' placeholder="Activity">
 			  	</div>
 			  	<div class ="col-sm-3">
 				  	<h5>Class</h5>
-				  	<input type='text' id='RAClass' name='RAClass'>
+				  	<input type='text' id='RAClass' name='RAClass' placeholder="Class">
 			  	</div>
 			  	<div class ="col-sm-3">
 				  	<h5>Project ID</h5>
-				  	<input type='text' id='RAId' name='RAId'>
+				  	<input type='text' id='RAId' name='RAId' placeholder="Project ID">
 			  	</div>
 		  	</div>
 
@@ -151,135 +234,136 @@
 
 		</div>
 
-		<div class = "row">
+		<div class = "row" style="text-align:center">
 			<div class ="col-sm-4">
 				<h5>Academic Year</h5>
-				<input type="text" required name="year"><br>
+				<input type="text" required name="year" placeholder="Academic Year"><br>
 			</div>
 			<div class ="col-sm-4">
 				<h5>Quarter</h5>
-				<input type="text" required name="quarter"><br>
+				<input type="text" required name="quarter" placeholder="Quarter"><br>
 			</div>
 			<div class ="col-sm-4">
 				<h5>Hiring Dept/Pgm</h5>
-				<input type="text" required name="hdept"><br>
+				<input type="text" required name="hdept" placeholder="Hiring Department"><br>
 			</div>
 		</div>
-
-		<br>
+        <br>
 		<div class = "divider"> </div>
 
 		<h3>Courses</h3>
 
-        <div class = "row">
+        <div class = "row" style="text-align:center">
         	<div class = "col-sm-4">
-        		<h5>Course ID</h5><br>
-				1. <input type="text" name="cid1">
+        		<h5>Course ID</h5>
+				1. <input type="text" name="cid1" id="cid1" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<h5>Course Title</h5><br>
-				<input type="text" name="ctitle1">
+				<h5>Course Title</h5>
+				<input type="text" name="ctitle1" id="ctitle1" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
-				<h5>Number of Credits</h5><br>
-				<input type="text" name="cc" onblur="findTotal()" id="cc1">
+				<h5>Number of Credits</h5>
+				<input type="text" name="cc" oninput="findTotal()" id="cc1" placeholder="Credits">
                 <input type="hidden" name="credit1" id="credit1">
 			</div>
+            <br>
 		</div>
+        <br>
 
-		 <br>
-
-		 <div class = "row">
+		 <div class = "row" id="class_2" style="display:none">
         	<div class = "col-sm-4">
-				2. <input type="text" name="cid2">
+				2. <input type="text" name="cid2" id="cid2" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle2">
+				<input type="text" name="ctitle2" id="ctitle2" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="cc" onblur="findTotal()" id="cc2">
+				<input type="text" name="cc" oninput="findTotal()" id="cc2" placeholder="Credits">
                 <input type="hidden" name="credit2" id="credit2">
 			</div>
 		</div>
+        <br id="class_2_break" style="display:none">
 
-		<br>
 
-		 <div class = "row">
+		 <div class = "row" id="class_3" style="display:none">
         	<div class = "col-sm-4">
-				3. <input type="text" name="cid3">
+				3. <input type="text" name="cid3" id="cid3" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle3">
+				<input type="text" name="ctitle3" id="ctitle3" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="cc" onblur="findTotal()" id="cc3">
+				<input type="text" name="cc" oninput="findTotal()" id="cc3" placeholder="Credits">
                 <input type="hidden" name="credit3" id="credit3">
 			</div>
 		</div>
+        <br id="class_3_break" style="display:none">
 
-		<br>
 
-		 <div class = "row">
+		 <div class = "row" id="class_4" style="display:none">
         	<div class = "col-sm-4">
-				4. <input type="text" name="cid4">
+				4. <input type="text" name="cid4" id="cid4" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle4">
+				<input type="text" name="ctitle4" id="ctitle4" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="cc" onblur="findTotal()" id="cc4">
+				<input type="text" name="cc" oninput="findTotal()" id="cc4" placeholder="Credits">
                 <input type="hidden" name="credit4" id="credit4">
 			</div>
 		</div>
+        <br id="class_4_break" style="display:none">
 
-		<br>
 
-		 <div class = "row">
+		 <div class = "row" id="class_5" style="display:none">
         	<div class = "col-sm-4">
-				5. <input type="text" name="cid5">
+				5. <input type="text" name="cid5" id="cid5" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle5">
+				<input type="text" name="ctitle5" title="ctitle5" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="cc" onblur="findTotal()" id="cc5">
+				<input type="text" name="cc" oninput="findTotal()" id="cc5" placeholder="Credits">
                 <input type="hidden" name="credit5" id="credit5">
 			</div>
 		</div>
+        <br id="class_5_break" style="display:none">
 
-		<br>
 
-		 <div class = "row">
+		 <div class = "row" id="class_6" style="display:none">
         	<div class = "col-sm-4">
-				6. <input type="text" name="cid6">
+				6. <input type="text" name="cid6" id="cid6" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle6">
+				<input type="text" name="ctitle6" id="ctitle6" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="cc" onblur="findTotal()" id="cc6">
+				<input type="text" name="cc" oninput="findTotal()" id="cc6" placeholder="Credits">
                 <input type="hidden" name="credit6" id="credit6">
 			</div>
 		</div>
-
-		<br>
+        <br id="class_6_break" style="display:none">
+        <div style="text-align:center;">
+            <button type="button" id="add_class" onClick="addClass()" style="display:inline">+</button>
+            <button type="button" id="add_class" onClick="removeClass()" style="display:inline">-</button>
+        </div>
+        <br>
 		<div class = "divider"> </div>
-
-		<input type="checkbox" required name="engineerTotal" id="engineerTotal" onClick="engrCheck()" value="true">
+        <br>
+        <div style="text-align:center">
+            TOTAL: $ <input type="text" name="total" id="total" value="0" style="display:inline"><br><br>
+        </div>
+		<input type="checkbox" required name="engineerTotal" id="engineerTotal" onClick="engrCheck()" value="true"> &nbsp;
 		<label for="engineerTotal"> Engineering Design Center and Student Association Fee ($150 per quarter) </label><br>
-        <script type="text/javascript">
-
-        </script>
-		TOTAL: $ <input type="text" name="total" id="total" value="0"><br><br>
-
 		<input type="checkbox" required name="tandc" id="tandc" value="true"> &nbsp;
 		<label for="tandc"> All information provided is correct to my knowledge.</label><br><br>
 
 		<b>Student's Signature</b>
-		<input type="text" name="stusign"> &nbsp;
+		<input type="text" name="stusign" placeholder="Student Signature"> &nbsp;
 
 		<b>Date</b>
-		<input type="text" name="studate"> &nbsp;
+		<input type="text" name="studate" placeholder="Date"> &nbsp;
 
 		<input type="submit" value="Submit"><br>
 

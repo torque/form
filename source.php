eb6df9ce0f1febe2ce01f43b289a47958b32b807
Marton Demeter <marton1029@gmail.com>
2016-11-20 20:21:30 -0800

    input validation fully functional

--- source.php
+++ source.php
@@ -10,13 +10,9 @@
 	</head>
 
 	<body>
-
-		<div class = "header-bar">
-
-			<img src="scu.jpg" alt="Santa Clara Uniersity">
-
-		</div>
-
+        <div style="width:100%;background-color:#F8F8F8">
+    		<img src="scu.jpg" alt="Santa Clara Uniersity" style="display:block;margin:auto;width:20%;height:20%;">
+        </div>
 		<form action="process_form.php" method="post">
 
 		<center><h2><u>Tuition and Fees Payment Authorization for Graduate Students</u></h2></center>
@@ -26,11 +22,11 @@
 		<div class = "row">
 			<div class ="col-sm-3">
 				<h5>First Name</h5>
-				<input type="text" required name="first_name" placeholder="First Name" id="first_name" oninput="setName()"><br>
+				<input type="text" required name="first_name" placeholder="First Name" id="first_name" oninput="setName(this.id)"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>Last Name</h5>
-				<input type="text" required name="last_name" placeholder="Last Name" id="last_name" oninput="setName()"><br>
+				<input type="text" required name="last_name" placeholder="Last Name" id="last_name" oninput="setName(this.id)"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>SCU ID</h5>
@@ -38,7 +34,7 @@
 			</div>
 			<div class ="col-sm-3">
 				<h5>Major</h5>
-				<input type="text" required name="major" placeholder="Major"><br>
+				<input type="text" required name="major" placeholder="Major" id="major" oninput="checkLetters(this.id,true)"><br>
 			</div>
 		</div>
 
@@ -49,7 +45,7 @@
 			</div>
 			<div class ="col-sm-3">
 				<h5>Advisor</h5>
-				<input type="text" required name="advisor" placeholder="Advisor"><br>
+				<input type="text" required name="advisor" placeholder="Advisor" id="advisor" oninput="checkLetters(this.id,true)"><br>
 			</div>
 			<div class ="col-sm-3">
 				<h5>Advisor Email</h5>
@@ -64,14 +60,23 @@
 
 		<script type="text/javascript">
             function setTheDate() {
+                var element = document.getElementById('studate');
                 var dt = new Date();
                 var day = dt.getDate();
                 var month = parseInt(dt.getMonth()) + 1;
                 var year = dt.getFullYear();
-                var val = month+"/"+day+"/"+year;
-                document.getElementById('studate').value = val;
+                if(element.type == 'text') {
+                    var val = month+"/"+day+"/"+year;
+                    document.getElementById('studate').value = val;
+                    return;
+                }
+                if(element.type == 'date') {
+                    var val = year+"-"+month+"-"+day;
+                    document.getElementById('studate').value = val;
+                }
             }
-            function setName() {
+            function setName(id) {
+                checkLetters(id);
                 var fname = document.getElementById('first_name').value;
                 var lname = document.getElementById('last_name').value;
                 document.getElementById('stusign').value = fname+" "+lname;
@@ -83,7 +88,9 @@
                 }
                 if(document.getElementById('engineerTotal').checked) {
                     document.getElementById('total').value = val + 150;
+                    document.getElementById('engineerTotal').value = "true";
                 } else {
+                    document.getElementById('engineerTotal').value = "false";
                     if(val > 150) {
                         document.getElementById('total').value -= "150";
                     } else {
@@ -114,6 +121,53 @@
                         }
                     } else {
                         break;
+                    }
+                }
+                document.getElementById(id).value = prev_val;
+            }
+            function checkDate(id) {
+                var val = document.getElementById(id).value;
+                var prev_val = "";
+                var regex = /[^0-9/]/
+                for(var i = 0; i < val.length; i++) {
+                    if(regex.test(val[i])) {
+                        break;
+                    } else {
+                        prev_val += val[i];
+                    }
+                }
+                document.getElementById(id).value = prev_val;
+            }
+            function checkLetters(id,space=false) {
+                var val = document.getElementById(id).value;
+                var prev_val = "";
+                if(space) {
+                    var regex = /[^a-zA-Z ]/
+                } else {
+                    var regex = /[^a-zA-Z]/
+                }
+                for(var i = 0; i < val.length; i++) {
+                    if(regex.test(val[i])) {
+                        break;
+                    } else {
+                       prev_val += val[i];
+                    }
+                }
+                document.getElementById(id).value = prev_val;
+            }
+            function checkAlphaNumeric(id,space=false) {
+                var val = document.getElementById(id).value;
+                var prev_val = "";
+                if(space) {
+                    var regex = /[^a-zA-Z0-9 ]/
+                } else {
+                    var regex = /[^a-zA-Z0-9]/
+                }
+                for(var i = 0; i < val.length; i++) {
+                    if(regex.test(val[i])) {
+                        break;
+                    } else {
+                       prev_val += val[i];
                     }
                 }
                 document.getElementById(id).value = prev_val;
@@ -195,13 +249,11 @@
                 }
             }
     	</script>
-
 		<label for="TA" style="font-weight: normal">TA</label>
-        <input type="radio" onclick="yesnoCheck()" name="atype" id="TA" value="TA" style="display:inline">
+        <input type="radio" required onclick="yesnoCheck()" name="atype" id="TA" value="TA" style="display:inline">
 		<label for="RA" style="font-weight: normal">RA</label>
         <input type="radio" onclick="yesnoCheck()" name="atype" id="RA" value="RA" style="display:inline">
         <input type="hidden" name="is_ra" value="false" id="is_ra"/>
-        
         <div id="TAYes" style="display:none">
             <div class="row">
                <div class ="col-sm-3">
@@ -220,33 +272,33 @@
 			<div class = "row">
 				<div class ="col-sm-3">
 					<h5>Account</h5>
-					<input type='text' id='RAAcc' name='RAAcc' placeholder="Account">
+					<input type='text' required id="RAAcc" oninput="checkLetters(this.id,true)" name='RAAcc' placeholder="Account">
 				</div>
 				<div class ="col-sm-3">
 					<h5>Fund</h5>
-				  	<input type='text' id='RAFund' name='RAFund' placeholder="Fund">
+				  	<input type='text' required id="RAFund" oninput="checkLetters(this.id,true)" name='RAFund' placeholder="Fund">
 			  	</div>
 			  	<div class ="col-sm-3">
 					<h5>Dept.</h5>
-				  	<input type='text' id='RADept' name='RADept' placeholder="Department">
+				  	<input type='text' required id="RADept" oninput="checkLetters(this.id,true)" name='RADept' placeholder="Department">
 			  	</div>
 			  	<div class ="col-sm-3">
 				  	<h5>Prgm. Code</h5>
-				  	<input type='text' id='RAcode' name='RAcode' placeholder="Prgm. Code">
+				  	<input type='text' required id="RAcode" oninput="checkAlphaNumeric(this.id,true)" name='RAcode' placeholder="Prgm. Code">
 			  	</div>
 		 	</div>
 		 	<div class ="row">
 			  	<div class ="col-sm-3">
 				  	<h5>Activity</h5>
-				  	<input type='text' id='RAAct' name='RAAct' placeholder="Activity">
+				  	<input type='text' required id="RAAct" oninput="checkLetters(this.id,true)" name='RAAct' placeholder="Activity">
 			  	</div>
 			  	<div class ="col-sm-3">
 				  	<h5>Class</h5>
-				  	<input type='text' id='RAClass' name='RAClass' placeholder="Class">
+				  	<input type='text' required id="RAClass" oninput="checkAlphaNumeric(this.id)" name='RAClass' placeholder="Class">
 			  	</div>
 			  	<div class ="col-sm-3">
 				  	<h5>Project ID</h5>
-				  	<input type='text' id='RAId' name='RAId' placeholder="Project ID">
+				  	<input type='text' required id="RAId" oninput="checkId(this.id)" name='RAId' placeholder="Project ID">
 			  	</div>
 		  	</div>
 
@@ -258,15 +310,15 @@
 		<div class = "row">
 			<div class ="col-sm-4">
 				<h5>Academic Year</h5>
-				<input type="text" required name="year" placeholder="Academic Year"><br>
+				<input type="text" required name="year" id="year" oninput="checkId(this.id,4)" placeholder="Academic Year"><br>
 			</div>
 			<div class ="col-sm-4">
 				<h5>Quarter</h5>
-				<input type="text" required name="quarter" placeholder="Quarter"><br>
+				<input type="text" required name="quarter" id="quarter" oninput="checkLetters(this.id)" placeholder="Quarter"><br>
 			</div>
 			<div class ="col-sm-4">
 				<h5>Hiring Dept/Pgm</h5>
-				<input type="text" required name="hdept" placeholder="Hiring Department"><br>
+				<input type="text" required name="hdept" id="hdept" oninput="checkLetters(this.id,true)" placeholder="Hiring Department"><br>
 			</div>
 		</div>
         <br>
@@ -288,15 +340,15 @@
         <div class = "row">
         	<div class = "col-sm-4">
         		<h5>Course ID</h5>
-				1. <input type="text" name="cid1" id="cid1" oninput="checkId(this.id,5)" placeholder="ID">
+				1. <input type="text" required name="cid1" id="cid1" oninput="checkId(this.id,5)" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
 				<h5>Course Title</h5>
-				<input type="text" name="ctitle1" id="ctitle1" placeholder="Title">
+				<input type="text"  required name="ctitle1" id="ctitle1" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
 				<h5>Number of Credits</h5>
-				<input type="text" name="credit1" oninput="findTotal()" id="credit1" placeholder="Credits" class="cc">
+				<input type="text" required name="credit1" oninput="findTotal()" id="credit1" placeholder="Credits" class="cc">
 			</div>
             <br>
 		</div>
@@ -307,7 +359,7 @@
 				2. <input type="text" name="cid2" id="cid2" oninput="checkId(this.id,5)" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle2" id="ctitle2" placeholder="Title">
+				<input type="text" name="ctitle2" id="ctitle2" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
 				<input type="text" name="credit2" oninput="findTotal()" id="credit2" placeholder="Credits" class="cc">
@@ -321,7 +373,7 @@
 				3. <input type="text" name="cid3" id="cid3" oninput="checkId(this.id,5)" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle3" id="ctitle3" placeholder="Title">
+				<input type="text" name="ctitle3" id="ctitle3" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
 				<input type="text" name="credit3" oninput="findTotal()" id="credit3" placeholder="Credits" class="cc">
@@ -335,7 +387,7 @@
 				4. <input type="text" name="cid4" id="cid4" oninput="checkId(this.id,5)" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle4" id="ctitle4" placeholder="Title">
+				<input type="text" name="ctitle4" id="ctitle4" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
 				<input type="text" name="credit4" oninput="findTotal()" id="credit4" placeholder="Credits" class="cc">
@@ -349,7 +401,7 @@
 				5. <input type="text" name="cid5" id="cid5" oninput="checkId(this.id,5)" placeholder="ID">
 			</div>
 			<div class = "col-sm-4">
-				<input type="text" name="ctitle5" id="ctitle5" placeholder="Title">
+				<input type="text" name="ctitle5" id="ctitle5" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
 			</div>
 			<div class = "col-sm-4">
 				<input type="text" name="credit5" oninput="findTotal()" id="credit5" placeholder="Credits" class="cc">
@@ -362,7 +414,7 @@
         		6. <input type="text" name="cid6" id="cid6" oninput="checkId(this.id,5)" placeholder="ID">
         	</div>
         	<div class = "col-sm-4">
-        		<input type="text" name="ctitle6" id="ctitle6" placeholder="Title">
+        		<input type="text" name="ctitle6" id="ctitle6" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
         	</div>
         	<div class = "col-sm-4">
         		<input type="text" name="credit6" oninput="findTotal()" id="credit6" placeholder="Credits" class="cc">
@@ -376,7 +428,7 @@
                 7. <input type="text" name="cid7" id="cid7" oninput="checkId(this.id,5)" placeholder="ID">
             </div>
             <div class = "col-sm-4">
-                <input type="text" name="ctitle7" id="ctitle7" placeholder="Title">
+                <input type="text" name="ctitle7" id="ctitle7" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
             </div>
             <div class = "col-sm-4">
                 <input type="text" name="credit7" oninput="findTotal()" id="credit7" placeholder="Credits" class="cc">
@@ -389,7 +441,7 @@
                 8. <input type="text" name="cid8" id="cid8" oninput="checkId(this.id,5)" placeholder="ID">
             </div>
             <div class = "col-sm-4">
-                <input type="text" name="ctitle8" id="ctitle8" placeholder="Title">
+                <input type="text" name="ctitle8" id="ctitle8" oninput="checkAlphaNumeric(this.id,true)" placeholder="Title">
             </div>
             <div class = "col-sm-4">
                 <input type="text" name="credit8" oninput="findTotal()" id="credit8" placeholder="Credits" class="cc">
@@ -398,7 +450,7 @@
         <br id="class_8_break" style="display:none">
 
 
-        <div style="text-align:center;">
+        <div style="text-align:center">
             <button type="button" id="add_class" onClick="addClass()" style="display:inline">+</button>
             <button type="button" id="remove_class" onClick="removeClass()" style="display:inline">-</button>
         </div>
@@ -406,20 +458,20 @@
         <br>
 		<div class = "divider"> </div>
         <br>
-        <div>
+        <div style="text-align:center">
             <label for="total" style="font-weight: normal">Total: $</label>
             <input type="text" name="total" id="total" style="display:inline" placeholder="0"><br><br>
         </div>
-		<input type="checkbox" required name="engineerTotal" id="engineerTotal" onClick="engrCheck()" value="true"> &nbsp;
+		<input type="checkbox" name="engineerTotal" id="engineerTotal" onClick="engrCheck()"> &nbsp;
 		<label for="engineerTotal"> Engineering Design Center and Student Association Fee ($150 per quarter) </label><br>
 		<input type="checkbox" required name="tandc" id="tandc" value="true"> &nbsp;
 		<label for="tandc"> All information provided is correct to my knowledge.</label><br><br>
 
 		<label for="stusign" style="font-weight: normal">Student's Signature</label>
-		<input type="text" name="stusign" id="stusign" placeholder="Student Signature"> &nbsp;
+		<input type="text" required name="stusign" id="stusign" oninput="checkLetters(this.id,true)" placeholder="Student Signature"> &nbsp;
 
 		<label for="studate" style="font-weight: normal">Date</label>
-		<input type="text" name="studate" id="studate" placeholder="Date"> &nbsp;
+		<input type="text" required name="studate" id="studate" oninput="checkDate(this.id)" placeholder="Date"> &nbsp;
         <script>setTheDate()</script>
 		<input type="submit" value="Submit"><br>
 
